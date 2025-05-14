343db52cca9e7505f991c38f7be0fcc892b7378f
Carlos <carlos@wasi.pro>
2025-05-14 10:28:07 -0500

    on shop adwdiget

--- lib/molecules/ad_widget.dart
+++ lib/molecules/ad_widget.dart
@@ -1,4 +1,3 @@
-import 'package:ecommerce_system_design/atoms/header_box.dart';
 import 'package:ecommerce_system_design/ecommerce_system_design.dart';
 import 'package:ecommerce_system_design/foundation/app_colors.dart';
 import 'package:ecommerce_system_design/foundation/app_typography.dart';
@@ -8,7 +7,14 @@ class AdWidget extends StatelessWidget {
   final String? label;
   final String? imageUrl;
   final String? price;
-  const AdWidget({super.key, required this.imageUrl, this.price, this.label});
+  final VoidCallback? onShop;
+  const AdWidget({
+    super.key,
+    required this.imageUrl,
+    this.price,
+    this.label,
+    this.onShop,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -25,26 +31,29 @@ class AdWidget extends StatelessWidget {
                 imageUrl != null ? Image.network(imageUrl ?? '') : SizedBox(),
           ),
           SizedBox(width: 10),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 150,
-                child: Text(
-                  label ?? '',
-                  maxLines: 1,
-                  textAlign: TextAlign.center,
-                  style: AppTypography.label.copyWith(
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.textLabel,
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 150,
+                  child: Text(
+                    label ?? '',
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                    style: AppTypography.label.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.textLabel,
+                    ),
                   ),
                 ),
-              ),
-              if (price != null)
-                CustomTitle(text: '\$$price', padding: EdgeInsets.zero),
-              SizedBox(height: 10),
-              if (price != null) CustomTextButton(text: '¡Shop now!'),
-            ],
+                if (price != null)
+                  CustomTitle(text: '\$$price', padding: EdgeInsets.zero),
+                SizedBox(height: 10),
+                if (price != null)
+                  CustomTextButton(text: '¡Shop now!', onTap: onShop),
+              ],
+            ),
           ),
         ],
       ),
